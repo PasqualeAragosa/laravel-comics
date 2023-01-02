0c0183a7c128aa92f68f0730288c62f6be9f4d5d
Pasquale Aragosa <aragosap@gmail.com>
2023-01-02 01:44:58 +0100

    add background images

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,65 +1,77 @@
 <footer>
-    <div class="info-box">
+    <div class="footer_top">
         <div class="container">
-            <div class="details">
-                <div class="col">
-                    <h4>DC Comics</h4>
-                    <ul>
-                        <li>Characters</li>
-                        <li>Comics</li>
-                        <li>Movies</li>
-                        <li>Tv</li>
-                        <li>Games</li>
-                        <li>Videos</li>
-                        <li>News</li>
-                    </ul>
-                    <h4>Shop</h4>
-                    <ul>
-                        <li>Shop DC</li>
-                        <li>Shop DC Collectibles</li>
-                    </ul>
+            <div class="row row-cols-2">
+                <div class="col py-4">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col px-0">
+                                <!-- dc comics -->
+                                <h5 class="text-uppercase">DC Comics</h5>
+
+                                <ul class="list-unstyled">
+                                    <li><a href="/characters">Characters</a></li>
+                                    <li><a href="/">Comics</a></li>
+                                    <li><a href="/movies">Movies</a></li>
+                                    <li><a href="/tv">TV</a></li>
+                                    <li><a href="/games">Games</a></li>
+                                    <li><a href="/videos">Videos</a></li>
+                                    <li><a href="/news">News</a></li>
+                                </ul>
+
+                                <!-- shop -->
+                                <h5 class="text-uppercase">Shop</h5>
+
+                                <ul class="list-unstyled">
+                                    <li><a href="/">Shop DC</a></li>
+                                    <li><a href="/collectibles">Shop DC Collectibles</a></li>
+                                </ul>
+                            </div>
+
+                            <div class="col px-0">
+                                <!-- dc -->
+                                <h5 class="text-uppercase">DC</h5>
+
+                                <ul class="list-unstyled">
+                                    <li><a href="/">Terms Of Use</a></li>
+                                    <li><a href="/">Privacy policy (New)</a></li>
+                                    <li><a href="/">Ad Choices</a></li>
+                                    <li><a href="/">Advertising</a></li>
+                                    <li><a href="/">Jobs</a></li>
+                                    <li><a href="/">Subscriptions</a></li>
+                                    <li><a href="/">Talent Workshops</a></li>
+                                    <li><a href="/">CPSC Certificates</a></li>
+                                    <li><a href="/">Ratings</a></li>
+                                    <li><a href="/">Shop Help</a></li>
+                                    <li><a href="/">Contact Us</a></li>
+                                </ul>
+                            </div>
+
+                            <div class="col px-0">
+                                <!-- sites -->
+                                <h5 class="text-uppercase">Sites</h5>
+
+                                <ul class="list-unstyled">
+                                    <li><a href="/">DC</a></li>
+                                    <li><a href="/">MAD Magazine</a></li>
+                                    <li><a href="/">DC Kids</a></li>
+                                    <li><a href="/">DC Universe</a></li>
+                                    <li><a href="/">DC Power Visa</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <!-- /.col -->
-                <div class="col">
-                    <h4>Dc</h4>
-                    <ul>
-                        <li>Term Of Use</li>
-                        <li>Privacy policy (New)</li>
-                        <li>Ad Choices</li>
-                        <li>Advertising</li>
-                        <li>Jobs</li>
-                        <li>Subscriptions</li>
-                        <li>Talent Woekshops</li>
-                        <li>CPSC Certificates</li>
-                        <li>Ratings</li>
-                        <li>Shop Help</li>
-                        <li>Contact Us</li>
-                    </ul>
-                </div>
-                <!-- /.col -->
-                <div class="col">
-                    <h4>DC Comics</h4>
-                    <ul>
-                        <li>DC</li>
-                        <li>MAD Magazine</li>
-                        <li>DC Kids</li>
-                        <li>DC Universe</li>
-                        <li>DC Power Visa</li>
-                    </ul>
-                </div>
-                <!-- /.col -->
+
+                <div class="col dc_logo_bg"></div>
             </div>
-            <!-- /.details -->
-            <div class="logo">
-            </div>
-            <!-- /.logo -->
         </div>
-        <!-- /.container -->
     </div>
-    <!-- /.info-box -->
-    <div class="footer-bottom">
+    <!-- /.footer_top -->
+
+    <div class="footer_bottom">
         <div class="container">
-            <div class="register">
+            <div class="register text-white">
                 <span>Sign Up Now!</span>
             </div>
             <!-- /.register -->
